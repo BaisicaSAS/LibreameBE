c106c05805c0e2f39bc3226837aeba77331299cc
Alex-Samsung <Alex-Samsung@Alex-Samsung-PC>
2014-08-27 00:49:50 -0500

    Version LibreameBE 27 Agosto: Madrugada

--- src/Libreame/BackendBundle/Controller/AccesoController.php
+++ src/Libreame/BackendBundle/Controller/AccesoController.php
@@ -8,12 +8,27 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
  * el acceso a todas las url. Esto implica la generacion y validacion de Tokens
  * Las funciones involucradas:
  * GenerarSesion / EliminarSesion / VerificarAcceso / DespacharOpcion / Cifrar-Descifrar comunicaciones
- * Alta y Bja de usuarios / Recuperacion y cambio de clave
+ * Alta y Baja de usuarios / Recuperacion y cambio de clave
  *  
  */
 class AccesoController extends Controller
 {
+    /*
+     * Index
+     */
     public function indexAction($name)
+    {
+        return $this->render('LibreameBackendBundle:Default:index.html.twig', array('name' => $name));
+    }
+    /*
+     * GeneraSesion 
+     * Devuelve el ID de la sesion
+     * Recibe una cadena con los datos del usuario
+     * Usuario/Password{cifrado}/FechaHora{Esta se guarda en el dispositivo para que sirva como clave}
+     * Id/nombre dispositivo
+     *  
+     */
+    public function generaSesionAction($credenciales)
     {
         return $this->render('LibreameBackendBundle:Default:index.html.twig', array('name' => $name));
     }
