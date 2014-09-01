311852d16062bfe180fb0e2bfd5fd12493ba517b
Alex-Samsung <Alex-Samsung@Alex-Samsung-PC>
2014-09-01 05:59:49 -0500

    Version LibreameBE 1 Septiembre: AM

--- src/Libreame/BackendBundle/Controller/AccesoController.php
+++ src/Libreame/BackendBundle/Controller/AccesoController.php
@@ -21,8 +21,30 @@ class AccesoController extends Controller
         return $this->render('LibreameBackendBundle:Default:index.html.twig', array('name' => $name));
     }
     /*
+     * Despacha opcion 
+     * La funcion recibe parametros desde la app: usr,pass,device,session,opcion a despachar
+     *  Devuelve los datos relacionados con la solicitud
+     */
+    public function despachaOpcionAction($credenciales)
+    {
+        return $this->render('LibreameBackendBundle:Default:index.html.twig', array('name' => $credenciales));
+    }
+    /*
+     * validaSesion 
+     * Valida los datos de la sesion verificando que sea veridica
+     * Credenciales está compuesto por: 1.usr,2.pass,3-device,4.session,5-opcion a despachar,
+     * parametros para la url a despachar, cantidad de caracteres de cada uno 
+     * de los anteriores cada uno con 4 digitos.
+     * 
+     */
+    public function validaSesionAction($credenciales)
+    {   
+        $tamanos = new (subst($credenciales, strlen($credenciales)-21));
+        
+    }
+    /*
      * GeneraSesion 
-     * Devuelve el ID de la sesion
+     * Guarda en BD y Devuelve el ID de la sesion
      * Recibe una cadena con los datos del usuario
      * Usuario/Password{cifrado}/FechaHora{Esta se guarda en el dispositivo para que sirva como clave}
      * Id/nombre dispositivo
@@ -30,6 +52,14 @@ class AccesoController extends Controller
      */
     public function generaSesionAction($credenciales)
     {
-        return $this->render('LibreameBackendBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('LibreameBackendBundle:Default:index.html.twig', array('name' => $credenciales));
+    }
+    /*
+     * eliminaSesion 
+     *  Elimina una sesion 
+     */
+    public function eliminaSesionAction($credenciales)
+    {
+        return $this->render('LibreameBackendBundle:Default:index.html.twig', array('name' => $credenciales));
     }
 }
