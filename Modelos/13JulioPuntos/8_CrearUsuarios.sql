19a4221c92575c0de12b1fc003f0639206b06ea1
mramirez <mramirez@MRAMIREZ.ESRICO.local>
2016-08-12 17:24:21 -0500

    12 Agosto 2016: Trabajos de migracion de datos

---
+++ Modelos/13JulioPuntos/8_CrearUsuarios.sql
@@ -0,0 +1,18 @@
+## 1. Crear lb_planesusuarios
+## 2. Crear lb_usuarios
+ 
+
+delete FROM dotex4read.lb_actsesion where inactsesion > 0;
+delete FROM dotex4read.lb_sesiones where insesion > 0;
+delete FROM dotex4read.lb_dispusuarios where indispusuario > 0;
+delete FROM dotex4read.lb_membresias where inmembresia > 0;
+delete FROM dotex4read.lb_usuarios where inusuario > 0;
+
+alter table dotEx4read.lb_actsesion  auto_increment=1;
+alter table dotEx4read.lb_sesiones  auto_increment=1;
+alter table dotEx4read.lb_dispusuarios  auto_increment=1;
+alter table dotEx4read.lb_membresias  auto_increment=1;
+alter table dotEx4read.lb_usuarios  auto_increment=1;
+
+SELECT * FROM dotex4read.lb_usuarios;
+commit;
---
+++ Modelos/13JulioPuntos/9_CrearProductos.sql
@@ -0,0 +1,14 @@
+## Para crear productos :: 
+
+## 1.Crear lb_titulos
+## 2.Crear lb_libros
+## 3.Crear lb_autoreslibros
+## 4.Crear lb_generoslibros
+## 5.Crear lb_editorialeslibros
+## 6.Crear lb_indicepalabra
+## 7.Crear lb_ejemplares
+## 8.Crear lb_histejemplar
+## 9.Crear lb_puntosusuario
+## 10.Todos los libros de Don Alex, deben estar marcados como solo venta / Los nuestros Cualquier tipo de transacción
+## 11.Respetar los libros activos e inactivos y las cuentas de los que he entregado deben tener los libros pero inactivos 
+    para que los usuarios puedan decidir si los publican.
